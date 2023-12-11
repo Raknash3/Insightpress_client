cf1729cc4718ef2c006f3b1d2374620a75681259
Ravi Shankar <ivarraknash3@gmail.com>
2023-12-10 21:09:40 -0500

    Local server URL

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
                 width={size}
                 height={size}
                 alt="user"
-                src={`https://insightpress-server.onrender.com/assets/${image}`}
+                src={`http://localhost:6001/assets/${image}`}
             />
         </Box>
     );
